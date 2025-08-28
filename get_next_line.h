dfb03876c35fd0d6b6859266cdad7dc41c6be086
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-27 23:24:42 -0300

    Inclui bibliotecas de write() e malloc().

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 19:19:52 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/27 19:27:54 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/27 23:22:53 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
