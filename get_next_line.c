55a97ce1235c04cb812f19be88be3ab069d6dc7e
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-30 16:31:13 -0300

    feat: implementa escrita com buffer definido de maneira estática e arbitrária.

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,20 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 19:17:50 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/30 15:49:15 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/30 16:27:33 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+//read, malloc, free
 char	*get_next_line(int fd)
 {
-	// CODE
-	return ((void *)0);
+	char	*str;
+	ssize_t	bytes_read;
+
+	str = malloc(20 * sizeof(char));
+	bytes_read = read(fd, str, 19);
+	str[bytes_read] = '\0';
+	return (str);
 }
