40c64efbb1ec83edd2e808c1712b868bbdca7b71
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-09-02 19:39:29 -0300

    feat: implementa controle de BUFFER_SIZE com valor default no sistema e também nos testes.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 19:17:50 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/31 20:20:49 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/09/02 19:38:51 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,9 @@ char	*get_next_line(int fd)
 	ssize_t	bytes_read;
 	int		buffer;
 
-	#ifdef BUFFER_SIZE
-		buffer = BUFFER_SIZE;
-	#else
-		buffer = 42;
-	#endif
-	str = malloc((buffer + 1) * sizeof(char));
-	bytes_read = read(fd, str, buffer);
+	printf("TESTE BUFFER => %d\n", BUFFER_SIZE);
+	str = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	bytes_read = read(fd, str, BUFFER_SIZE);
 	str[bytes_read] = '\0';
 	trimmed_line = trim_line(str);
 	printf("TESTE ===> %s\n", trimmed_line);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 19:19:52 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/31 20:07:47 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/09/02 19:15:23 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 3
+#endif
 
 char	*get_next_line(int fd);
 char	*trim_line(char *str);
